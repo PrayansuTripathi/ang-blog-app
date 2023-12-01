f49ade61f2fdb44387c26c067d4f6f7017b9c4a9
prayansu <prayansutripathi@gmail.com>
2023-12-01 14:13:39 +0530

    defaul path added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,8 @@ import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
 
 const routes: Routes = [
-  { path: "", component: HomeComponent },
+  { path: "", redirectTo: "home", pathMatch:"full" },
+  { path: "home", component: HomeComponent },
   { path: "category", component: SingleCategoryComponent },
   { path: "post", component: SinglePostComponent },
   { path: "about", component: AboutUsComponent },
